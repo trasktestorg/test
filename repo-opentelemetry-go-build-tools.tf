17cd8e6b37657f71918f283bd0fa0bee4633d3a1
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-06-04 20:55:59 -0700

    Add GC back as an org-level assignment (#56)
    * Add GC back as an org-level assignment
    
    * refine
    
    * refine

--- repo-opentelemetry-go-build-tools.tf
+++ repo-opentelemetry-go-build-tools.tf
@@ -45,6 +45,14 @@ resource "github_repository_collaborators" "opentelemetry-go-build-tools" {
   # to avoid false positives showing up in the plan
   # (see underlying bug at https://github.com/integrations/terraform-provider-github/issues/2445)
   team {
+    team_id = github_team.governance-committee.id
+    permission = "push"
+  }
+
+  # this is really an org-level role, but it needs to be specified here
+  # to avoid false positives showing up in the plan
+  # (see underlying bug at https://github.com/integrations/terraform-provider-github/issues/2445)
+  team {
     team_id = github_team.sig-security-maintainers.id
     permission = "pull"
   }
