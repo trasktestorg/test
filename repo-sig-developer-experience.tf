f0057c70f2862c6837f06880885f14ccb0f8400d
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-06-05 08:31:20 -0700

    Give sig-developer-experience approvers and maintainers permissions to their repo (#63)
    * Give sig developer experience approvers and maintainers permissions
    
    * Update repo-sig-developer-experience.tf

--- repo-sig-developer-experience.tf
+++ repo-sig-developer-experience.tf
@@ -10,6 +10,36 @@ module "repo-sig-developer-experience" {
   delete_branch_on_merge = false
 }
 
+resource "github_repository_collaborators" "sig-developer-experience" {
+  repository = "sig-developer-experience"
+
+  team {
+    team_id = github_team.sig-developer-experience-approvers.id
+    permission = "push"
+  }
+
+  team {
+    team_id = github_team.sig-developer-experience-maintainers.id
+    permission = "maintain"
+  }
+
+  # this is really an org-level role, but it needs to be specified here
+  # to avoid false positives showing up in the plan
+  # (see underlying bug at https://github.com/integrations/terraform-provider-github/issues/2445)
+  team {
+    team_id = github_team.governance-committee.id
+    permission = "push"
+  }
+
+  # this is really an org-level role, but it needs to be specified here
+  # to avoid false positives showing up in the plan
+  # (see underlying bug at https://github.com/integrations/terraform-provider-github/issues/2445)
+  team {
+    team_id = github_team.sig-security-maintainers.id
+    permission = "pull"
+  }
+}
+
 module "branch-protection-rule-sig-developer-experience-0" {
   source = "./modules/branch-protection-long-term"
   repository_id = module.repo-sig-developer-experience.node_id
